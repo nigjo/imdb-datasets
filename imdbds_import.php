38fcbc9257a6aa4b2d9ad3e2875337e075fb5e29
Jens Hofschröer <github@nigjo.de>
2021-01-24 14:17:18 +0100

    added determination of uncompressed file size. slows down the whole process a bit, but makes the progress accurate

--- imdbds_import.php
+++ imdbds_import.php
@@ -5,12 +5,33 @@ if(file_exists($dbfile)){
   unlink($dbfile);
 }
 
+function gzend($fh){
+  $d   = 1<<10;
+  $eof = 0;
+  while (false!==gzgetc($fh)){
+    $eof += $d;
+    gzseek($fh, $eof);
+  } 
+  while ( $d > 1 )
+  {
+    gzgetc($fh);
+    $d >>= 1;
+    $eof += $d * (gzeof($fh)? -1 : 1);
+    gzseek($fh, $eof);
+  }
+  gzrewind($fh);
+  return $eof;
+}
+
+
 function loadDataset($db, $filepath){
   echo $filepath.PHP_EOL;
   $count=0;
-  $filesize = filesize($filepath);
   $progress = 0;
   $in = gzopen($filepath, "r");
+  echo 'preparing file...'."\r";
+  $filesize = gzend($in);
+  $progressdelta = 1000;
   $tablename = str_replace('.','_',basename($filepath, '.tsv.gz'));
   $db->exec('BEGIN TRANSACTION');
   while(false!=($line=gzgets($in))){
@@ -30,22 +51,19 @@ function loadDataset($db, $filepath){
       $db->exec($q);
     }
     ++$count;
-    if($count % 100000 === 0){
-      // $db->exec('COMMIT TRANSACTION');
+    if($count%$progressdelta===0){
       $loadposition = ftell($in);
       $progress = floor(($loadposition / $filesize)*100);
-      // $db->exec('BEGIN TRANSACTION');
-    }
-    if($count%1000===0){
       if($progress>0)
         echo 'line '.$count.' - '.$progress."%\r";
       else
         echo 'line '.$count."\r";
     }
   }
+  echo 'line '.$count.' - 100%'."\r";
   $db->exec('COMMIT TRANSACTION');
   gzclose($in);
-  echo $count." Zeilen gelesen".PHP_EOL;
+  echo number_format($count)." lines read.".PHP_EOL;
 }
 
 $db = new SQLite3($dbfile);
