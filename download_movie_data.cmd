28fbfdeef866b690ee83cdf04e8d2e5a60449678
Jens Hofschröer <github@nigjo.de>
2021-01-24 10:04:44 +0100

    added download script

---
+++ download_movie_data.cmd
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+set baseurl=https://datasets.imdbws.com
+
+call :download name.basics.tsv.gz
+call :download title.akas.tsv.gz
+call :download title.basics.tsv.gz
+call :download title.crew.tsv.gz
+call :download title.principals.tsv.gz
+call :download title.ratings.tsv.gz
+echo --------------------------------------------------
+set dlstatus
+echo --------------------------------------------------
+
+pause
+goto :eof
+:download
+set "line=-----%~1---------------------------------------------"
+echo %line:~0,50%
+title download %1 - %~nx0
+set FROM=%baseurl%/%1
+set TO=data\%1
+set dlstatus_%1=OK
+powershell -C "$ProgressPreference = 'SilentlyContinue';Invoke-WebRequest -Uri %FROM% -OutFile %TO%;"
+if errorlevel 1 set set dlstatus_%1=%ERRORLEVEL%
